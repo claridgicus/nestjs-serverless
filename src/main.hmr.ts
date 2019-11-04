bc7042af8193bbf99d712b0e57a0066c1b4d3523
James Claridge <james@claridgeand.co>
2019-11-04 12:22:08 +1100

    improvements to hmr to accept the same .env setup

--- src/main.hmr.ts
+++ src/main.hmr.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: process.env.UI_URL,
+	});
+	await app.listen(process.env.EXPRESS_PORT);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+	if (module.hot) {
+		module.hot.accept();
+		module.hot.dispose(() => app.close());
+	}
 }
 bootstrap();
